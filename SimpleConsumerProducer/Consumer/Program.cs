6f400aee190f8f659202d6d4becc241e9da12822
Emircan <emircandv787@gmail.com>
2024-04-26 22:05:33 +0300

    Add project files.

---
+++ SimpleConsumerProducer/Consumer/Program.cs
@@ -0,0 +1,80 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Consumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory();
+
+            factory.Uri = new Uri("amqp:localhost:5672");
+            factory.ClientProvidedName = "Emircanpc2";
+
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+
+            channel.QueueDeclare(
+                queue: "example-queue",
+                exclusive: false);
+
+            //EventingBasicConsumer consumer = new(channel);
+
+            //consumer.Received += (sender, e) =>
+            //{
+            //    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+
+            //};
+
+
+            //channel.BasicConsume(
+            //    queue: "example-queue",
+            //    autoAck: false,
+            //    consumer: consumer);
+
+
+            channel.QueueConsume(
+            queueName: "example-queue",
+            action: (message) =>
+            {
+                Console.WriteLine(message);
+            });
+
+
+            Console.Read();
+
+        }
+
+
+    }
+    public static class Exto
+    {
+
+        public static void QueueConsume(this IModel channel, string queueName, Action<string> action)
+        {
+            EventingBasicConsumer consumer = new(channel);
+
+            consumer.Received += (sender, e) =>
+            {
+                action(e.Body.Span.ByteSpanToString());
+            };
+
+
+            channel.BasicConsume(
+                queue: queueName,
+                autoAck: false,
+                consumer);
+
+        }
+
+        private static string ByteSpanToString(this ReadOnlySpan<byte> span)
+            => Encoding.UTF8.GetString(span);
+
+
+    }
+
+
+}
---
+++ SimpleConsumerProducer/Publisher/Program.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace Publisher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory();
+
+            factory.Uri = new Uri("amqp:localhost:5672");
+            factory.ClientProvidedName = "Emircanpc";
+
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+
+            //durable : kuyruktaki mesajların kalıcılığı 
+            //exclusive bu kuyrugun özel olup olmadıgı yani birden fazla bağlantı ile işlem yapılıp yapılamayacağı ile ilgili
+
+            channel.QueueDeclare(
+                queue: "example-queue",
+                exclusive: false);
+
+            var message = Encoding.UTF8.GetBytes("mesaj");
+
+            channel.BasicPublish(
+                exchange: string.Empty,
+                routingKey: "example-queue",
+                body: message);
+
+            
+            Console.Read();
+
+        }
+    }
+}
